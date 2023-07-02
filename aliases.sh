f5ddeddb38e9e2adad2baaad0cbd20528eeb51a6
Chris Bovers <chris@brik.digital>
2023-07-02 23:16:23 +0200

    Changes

--- aliases.sh
+++ aliases.sh
@@ -1,21 +1,8 @@
+# General
+alias c='clear'
 alias t='tail -f'
 alias dud='du -d 1 -h'
-
 alias refresh='source ~/.zshrc; echo "Reloaded .zshrc."'
-
-alias ga="git add"
-alias gaa="git add ."
-alias gc="git commit -m"
-alias gs="git status"
-alias gf="git fetch"
-alias gp="git push"
-alias gpf="git push --force-with-lease --force-if-includes"
-
-alias glf="git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-alias gl="glf | head"
-alias glz='git log --oneline | fzf --preview "git show {+1} --color=always" | cut -c 1-8'
-
-alias nah="git reset --hard && git clean -df"
 
 alias ip="curl icanhazip.com"
 alias ipv4="curl ipv4.icanhazip.com"
@@ -23,3 +10,19 @@ alias localip="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{
 alias getip="dig +short myip.opendns.com @resolver1.opendns.com"
 
 alias sshkey="cat ~/.ssh/id_ed25519.pub | pbcopy && echo 'Copied to clipboard.'"
+
+# Directories
+alias sites="cd ~/Sites"
+
+# Git
+alias ga="git add"
+alias gaa="git add ."
+alias gc="git commit -m"
+alias gs="git status"
+alias gf="git fetch"
+alias gp="git push"
+alias gpf="git push --force-with-lease --force-if-includes"
+alias glf="git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias gl="glf | head"
+alias glz='git log --oneline | fzf --preview "git show {+1} --color=always" | cut -c 1-8'
+alias nah="git reset --hard && git clean -df"
